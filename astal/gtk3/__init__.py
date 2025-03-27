c3bc1b85d0838e00d7b9474c369207b576d5af0b
nat <natalie-roentgen-connolly@mail.natalie-roentgen-connolly.website>
2025-03-27 14:44:03 -0700

    update gtk3 Widget to be a module instead of enum

--- astal/gtk3/__init__.py
+++ astal/gtk3/__init__.py
@@ -6,7 +6,7 @@ gi.require_version("Astal", "3.0")
 
 from astal.gtk3.app import App
 from astal.gtk3.astalify import astalify
-from astal.gtk3.widget import Widget
+import astal.gtk3.widget as Widget
 
 from gi.repository import Gtk, Gdk, Astal
 
